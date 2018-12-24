e831d5ddc59faaaa9622c874d036817d0224bc80
Your Name <you@example.com>
2018-12-23 22:48:28 -0500

    added verified hmac files

--- sel4-camkes-proj/hello-camkes-1/components/Crypto/src/crypto.c
+++ sel4-camkes-proj/hello-camkes-1/components/Crypto/src/crypto.c
@@ -1,11 +1,17 @@
 /* crypto.c */
 
 #include <camkes.h>
+#include <string.h>
 
 const char* SOURCE = "crypto";
-const char* HMAC = "JOAWPIDKJWOPADKASDOVSDKRGVBRDGPOK";
+char* KEY = "Jad23@(TGJGJEJ(FS39pf&%";
 void crypto_iface_sign(const char* text, const int id){
-	signtx_iface_print_sign(text, SOURCE, id, 1, HMAC); 
+	char hmac[64];
+	char text_[sizeof(text)];
+	strncpy(text_, text, sizeof(text));
+	HMAC2(KEY,sizeof(KEY), text_, sizeof(text_), hmac);
+	printf("hmac: %s\n", hmac);
+	signtx_iface_print_sign(text, SOURCE, id, 1, hmac); 
 }
 
 void crypto_iface_verify(const char* text, const int id, const char* hmac){
